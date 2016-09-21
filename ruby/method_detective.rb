dd97d4c73d9f1236f55cfd2ec274a1f8c1d1b33c
Alina Din <alinadin09@gmail.com>
2016-09-21 15:24:00 -0700

    Paste empty question set

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,38 +4,42 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
-=> “InVeStIgAtIoN”
+# "iNvEsTiGaTiOn".<???>
+# => “InVeStIgAtIoN”
 
-"zom".insert(2, 'o')
-=> “zoom”
+# "zom".<???>
+# => “zoom”
 
-"enhance".rjust(11). ljust(15)
-"    enhance    "
+# "enhance".<???>
+# => "    enhance    "
 
-"Stop! You’re under arrest!".upcase
-=> "STOP! YOU’RE UNDER ARREST!"
+# "Stop! You’re under arrest!".<???>
+# => "STOP! YOU’RE UNDER ARREST!"
 
-a = "the usual "
-a << "suspects"
-=> "the usual suspects"
+# "the usual".<???>
+#=> "the usual suspects"
 
 # " suspects".<???>
 # => "the usual suspects"
 
-"The case of the disappearing last letter".chomp("r")
-"The case of the disappearing last letter".chop
-=> "The case of the disappearing last lette"
+# "The case of the disappearing last letter".<???>
+# => "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".delete "T"
-=> "he mystery of the missing first letter"
+# "The mystery of the missing first letter".<???>
+# => "he mystery of the missing first letter"
 
-"Elementary,    my   dear        Watson!".squeeze
-=> "Elementary, my dear Watson!"
+# "Elementary,    my   dear        Watson!".<???>
+# => "Elementary, my dear Watson!"
 
-"z".ord
-=> 122 
-(What is the significance of the number 122 in relation to the character z?)
+# "z".<???>
+# => 122 
+# (What is the significance of the number 122 in relation to the character z?)
 
-a = "How many times does the letter 'a' appear in this string?"
-a.count "a"
+# "How many times does the letter 'a' appear in this string?".<???>
+# => 4
+
+# Replace in the "<???>" with the appropriate method (and arguments, if any).
+# Uncomment the calls to catch these methods red-handed.
+
+# When there's more than one suspect who could have
+# committed the crime, add additional calls to prove it.
