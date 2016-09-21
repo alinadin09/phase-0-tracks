b1d22e08f6d6449827cb11bf6fae5fcb524443b9
Alina Din <alinadin09@gmail.com>
2016-09-21 12:09:07 -0700

    Update algos.js

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,5 @@
 // figure out the length of each phrase
-// SORT by length from least to greatest
+// SORT by length from greatest to least
 // return the phrase at index 0.
 
 
@@ -18,3 +18,20 @@ function namelength(arr) {
 }
 
 namelength(names);
+
+// var names = ["alina", "alina mehvesh", "alina mehvesh din"]
+
+
+// function what(arr) {
+// 	function namelength() {
+// 		var i;
+// 		for (i = 0; i < arr.length; i++) { 
+// 			console.log(arr[i].length);
+// 		}
+// 	}
+// namelength.sort;
+
+// }
+
+
+// what(names)
