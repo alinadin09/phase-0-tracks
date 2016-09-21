56ef18e43fc66eead5ee1f24d97a5ea2d98c3bf9
Alina Din <alinadin09@gmail.com>
2016-09-20 19:20:25 -0700

    Is able to return element lengths

---
+++ js/algos.js
@@ -0,0 +1,20 @@
+// figure out the length of each phrase
+// SORT by length from least to greatest
+// return the phrase at index 0.
+
+
+
+var names = ["alina", "alina mehvesh", "alina mehvesh din"]
+
+function namelength(arr) {
+	
+	
+	var i;
+	
+	for (i = 0; i < arr.length; i++) { 
+		console.log(arr[i].length);
+	}
+
+}
+
+namelength(names);
