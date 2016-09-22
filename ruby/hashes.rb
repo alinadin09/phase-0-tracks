ac26499662571dde33f07d7d7c9fe3382a93c5e1
Alina Din <alinadin09@gmail.com>
2016-09-21 22:56:02 -0700

    Finish 5.2

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,35 +1,59 @@
+# Ask the user for basic information and store the answers and values.
+
 puts "Please enter the following information as it appears on your screen:"
 puts "What is your name?"
-name = gets.chomp.to_s
+name = gets.chomp
 
 puts "What is your email?"
-email = gets.chomp.to_s
+email = gets.chomp
 
 puts "What is your phone number?"
-phone = gets.chomp.to_s
+phone = gets.chomp.to_i
 
 puts "Is ombre cool?"
-ombre_is_cool = gets.chomp.to_s
+ombre = gets.chomp
+
+# Feed the values into a hash, and give them keys:
 
 job_app = {
 
 	name: name,
 	email: email,
-	phone: phone.to_i,
-	ombre_is_cool: ombre_is_cool.to_b
+	phone: phone,
+	ombre: ombre
 }
 
 puts "Here is your completed application:"
 p job_app
 
-loop do
-break if input == "no" 
-	
-	puts "Do you want to update anything? (type yes/no)"
-	input = gets.chomp
+# Ask the user if she wants to update:
+
+puts "Do you want to update anything? (type yes/no)"
+input = gets.chomp
 	if input == "yes"
-		puts "what do you want to update?"
-		update = gets.chomp
-		if update == "name"
+		puts "what do you want to update?" # This part turns "update" into a value for the key.
+		update = gets.chomp.intern
+		puts "Please update name, email, phone, or ombre."
+		repsonse = gets.chomp
+			if update == :name
+				job_app[:name] = repsonse
+			elsif update == :email
+				job_app[:email] = repsonse
+			elsif update == :phone
+				job_app[:phone] = repsonse
+			elsif update == :ombre
+				job_app[:ombre] = repsonse
+			end		
+		
+		p job_app
+
+
+	elsif input == "no"
+		puts "Great! You're done."
+		p job_app
+	end
+
+
+		
 
 
