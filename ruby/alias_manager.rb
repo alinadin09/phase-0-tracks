a88bef9944eb0423289cccc50f5a63b1647c1fb2
Alina Din <alinadin09@gmail.com>
2016-09-23 14:06:21 -0700

    Got UI to work

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,55 +1,55 @@
 # iterate through the name;
 # if the letter is a vowel, create a new name "alias_name" which replaces each vowel with the next vowel. 
 # and replace each consonant with the next consonant. 
-
-
-
-
+	
 def encrypt_name(name)
+
+		vowels = ["a", "e", "i", "o", "u"]
+		consonants = ["b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+		alias_name = ""
+			
+			name.each_char do |letter|
+			# if the letter is inside the array called vowels
+				if vowels.include?(letter)
+					# replace the letter with the next vowel in the array
+					# put it into alias_name
+					alias_name << vowels[vowels.index(letter)+1]
+				# replace the letter with the next consonant in the array
+				# put it into alias_name
+				elsif consonants.include?(letter)
+					alias_name << consonants[consonants.index(letter)+1]
+				# put letter into alias_name
+				# this is for the spaces	
+				else
+					alias_name << letter	
+				end
+			end
+		
+		alias_name.split.reverse.map(&:capitalize)*' '
+
+end
+
+# Driver code / UI. Encapsulating a code to prompt user for name.
+
+def runner
+name_storage = {}
 
 	loop do 
 	puts "please enter your name (type quit if you want to exit the program)"
 	name = gets.chomp
 	break if name == 'quit'
 
-name_storage = {}
-
-name_storage[:real_name] = name
-
-	vowels = ["a", "e", "i", "o", "u"]
-	consonants = ["b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-	alias_name = ""
-	name.each_char do |letter|
-	# if the letter is inside the array called vowels
-		if vowels.include?(letter)
-
-			# replace the letter with the next vowel in the array
-			# put it into alias_name
-			alias_name << vowels[vowels.index(letter)+1]
+	name_storage[:real_name] = name
 		
-		# replace the letter with the next consonant in the array
-		# put it into alias_name
-		elsif consonants.include?(letter)
-			alias_name << consonants[consonants.index(letter)+1]
-		
-		# put letter into alias_name
-		# this is for the spaces	
-		else
-			alias_name << letter
-			
-		end
-	end
-	encrypted_name = alias_name.split.reverse.map(&:capitalize)*' '
+		puts "This is your alias name!"
+		p encrypt_name(name)
 
-	name_storage[:fake_id] = encrypted_name
+		name_storage[:fake_id] = encrypt_name(name)
 
+	end	
 
-	puts "This is your alias name!"
-	p encrypt_name(name)
-
+	puts "jk! #{name_storage[:fake_id]} is really #{name_storage[:real_name]}!"
 end
-
-
-end
+runner
 
 
