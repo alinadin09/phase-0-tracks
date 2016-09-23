28a5595fb11c508032084b6bc15141a6ac6fe0b2
Alina Din <alinadin09@gmail.com>
2016-09-22 21:36:25 -0700

    I'm trying again

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,68 +1,54 @@
-# Application needs to take in data from user about his name, age, address, preferences
-idesign_job_app = []
+# Ask the user for basic information and store the answers and values.
 
-# Ask the user the first question
-puts "Hello! Welcome to the interior design job application! Please enter your name."
-
-# Gather data from the user.
+puts "Please enter the following information as it appears on your screen:"
+puts "What is your name?"
 name = gets.chomp
-puts "How old are you?"
-age = gets.chomp.to_i
-puts "How many kids do you have?"
-kids = gets.chomp.to_i
-puts "What is your address?"
-address = gets.chomp
-puts "What is your decor theme?"
-theme = gets.chomp
 
-# Feed all gathered data into the program array:
-idesign_job_app << [name, age, address, kids, theme]
+puts "What is your email?"
+email = gets.chomp
 
-# initialize program
-idesign_job_app
+puts "What is your phone number?"
+phone = gets.chomp.to_i
 
-# print data collected for the interior design application:
-puts "Here is your completed application."
+puts "Is ombre cool?"
+ombre = gets.chomp
 
+# Feed the values into a hash, and give them keys:
 
-# store the data in keys
- idesign_job_app = {
-	:name => name,
-	:age => age,
-	:kids => kids,
- 	:address => address,
-	:theme => theme
- }
- 
- p idesign_job_app
- 
- # Allow user to update the key. He or she types the key as a string; which gets converted to a key. User is prompted to enter in new data for the key, which then updates the key. The hash is printed back to the user.
- 
- loop do
- puts "Would you like to update anything?"
- answer = gets.chomp.downcase
- break if answer == "no"
- 
-	 if answer == "yes"
-	 puts "Which section would you like to update? (name, age, kids, address, or theme?)"
-	 reply = gets.chomp.downcase
-	 
-	 	case reply 
-		 when "name" then puts "please update"
-		 idesign_job_app[:name] = gets.chomp
-		 when "age" then puts "please update"
-		 idesign_job_app[:age] = gets.chomp.to_i
-		 when "kids" then puts "please update"
-		 idesign_job_app[:kids] = gets.chomp.to_i
-		 when "address" then puts "please update"
-		 idesign_job_app[:address] = gets.chomp
-		 when "theme" then puts "please update"
-		 idesign_job_app[:theme] = gets.chomp
-		end
+job_app = {
+
+	name: name,
+	email: email,
+	phone: phone,
+	ombre: ombre
+}
+
+puts "Here is your completed application:"
+p job_app
+
+# Ask the user if she wants to update:
+
+puts "Do you want to update anything? (type yes/no)"
+input = gets.chomp
+	if input == "yes"
+		puts "what do you want to update?" # This part turns "update" into a value for the key.
+		update = gets.chomp.intern
+		puts "Please update name, email, phone, or ombre."
+		repsonse = gets.chomp
+			if update == :name
+				job_app[:name] = repsonse
+			elsif update == :email
+				job_app[:email] = repsonse
+			elsif update == :phone
+				job_app[:phone] = repsonse
+			elsif update == :ombre
+				job_app[:ombre] = repsonse
+			end		
 		
-	 end
-	 
-end
-  	
-puts "Great! You're done."
-puts idesign_job_app
+		p job_app
+
+
+	elsif input == "no"
+		puts "Great! You're done."
+		p job_app
+	end
--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,38 +1,38 @@
-# #Writing a method that takes a block:
+#Writing a method that takes a block:
 
-# 	def shopping_bag
-# 		puts "Check out my bag!"
-# 		yield("shampoo", "soap", "onions", "milk")
-# 	end
+	def shopping_bag
+		puts "Check out my bag!"
+		yield("shampoo", "soap", "onions", "milk")
+	end
 
-# 	shopping_bag { |item1, i2, i3, i4| puts "#{item1}, #{i2}, #{i3}, and #{i4} are so useful!" }
+	shopping_bag { |item1, i2, i3, i4| puts "#{item1}, #{i2}, #{i3}, and #{i4} are so useful!" }
 
-# #Release 1:
+#Release 1:
 
-# 	baby_names = ["Arya", "Zain", "Raheem", "Haqq"]
-# 	feeder = []
+	baby_names = ["Arya", "Zain", "Raheem", "Haqq"]
+	feeder = []
 
-# 	top_news = { 
+	top_news = { 
 
-# 		headliner:"Branglina broke up! :'(", 
-# 		sub_header:"It was Brad's fault", 
-# 		lower_corner:"Hillary Clinton did something", 
-# 		footer:"Trump sucks and we don't care anymore" 
+		headliner:"Branglina broke up! :'(", 
+		sub_header:"It was Brad's fault", 
+		lower_corner:"Hillary Clinton did something", 
+		footer:"Trump sucks and we don't care anymore" 
 
-# 	}
-# 	puts "Before modification:"
-# 	p baby_names
-# 	puts "After .each modification"
-# 	baby_names.each { |name| puts "These are the names I like: #{name}" }
-# 	puts "After .map! modification"
-# 	baby_names.map! { |name| p "Cute "  + "#{name}" }
-# 	p baby_names
+	}
+	puts "Before modification:"
+	p baby_names
+	puts "After .each modification"
+	baby_names.each { |name| puts "These are the names I like: #{name}" }
+	puts "After .map! modification"
+	baby_names.map! { |name| p "Cute "  + "#{name}" }
+	p baby_names
 
-# 	puts "Before modifying the hash:"
-# 	p top_news
+	puts "Before modifying the hash:"
+	p top_news
 
-# 	puts "After using .each on the hash:"
-# 	top_news.each { |ranking, name| puts "The #{ranking} is #{name}" }
+	puts "After using .each on the hash:"
+	top_news.each { |ranking, name| puts "The #{ranking} is #{name}" }
 
 # Release 2:
 
