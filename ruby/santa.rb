34525595e9a7f17ce000a6574fdb7cba8d4aa48f
Alina Din <alinadin09@gmail.com>
2016-09-29 11:38:02 -0700

    Finish 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,18 +1,19 @@
 class Santa
+	attr_reader :ethnicity
+	attr_accessor :gender, :age
 
 	def initialize(gender, ethnicity)
 		puts "Intializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
-		puts "gender is #{gender} and ethnicity is #{ethnicity}"
-	end
-
-	def reindeer_ranking
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
-	def age
-		@age = 0
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking.insert(-1, reindeer_name)
+		p @reindeer_ranking
 	end
 
 	def speak
@@ -23,22 +24,48 @@ class Santa
 		puts "That was a good #{cookie_type} cookie!"
 	end
 
+	def celebrate_birthday
+		@age += 1
+		puts "Santa's new age is #{@age}"
+	end
 end
+
+# UI / Driver Code
 
 # santa = Santa.new("girl", "Pakistani")
 # santa.speak
 # santa.eat_milk_and_cookies("sugar")
+# santa.get_mad_at("Rudolph")
+# santa.celebrate_birthday
+# p santa.gender = ("boy")
+# p santa.age
+# p santa.ethnicity
 
-santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+5.times do | santa |
+puts "---------"
+santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+santas_age = santa.age = rand(140)
+puts "santa's age is: #{santas_age}"
+puts "santa's gender is: " + santa.gender
+puts "santa's ethnicity is: " + santa.ethnicity
+end
 
 
-	santas << Santa.new("agender", "black")
-	santas << Santa.new("female", "Latino")
-	santas << Santa.new("bigender", "white")
-	santas << Santa.new("male", "Japanese")
-	santas << Santa.new("female", "prefer not to say")
-	santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-	santas << Santa.new("N/A", "N/A")
+
+
+# santas = []
+
+
+# 	santas << Santa.new("agender", "black")
+# 	santas << Santa.new("female", "Latino")
+# 	santas << Santa.new("bigender", "white")
+# 	santas << Santa.new("male", "Japanese")
+# 	santas << Santa.new("female", "prefer not to say")
+# 	santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# 	santas << Santa.new("N/A", "N/A")
 
 # puts "Creating a loop with each santa:"
 # santas.each do |santa|
