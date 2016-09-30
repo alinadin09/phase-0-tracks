ed8b8c1f319080870896a2e55a3033dce450f84e
Alina Din <alinadin09@gmail.com>
2016-09-30 15:46:10 -0700

    Write pseudocode

---
+++ ruby/game/Game.rb
@@ -0,0 +1,11 @@
+# pseudocode
+# correct_word is an ARRAY
+# working_word is an ARRAY that keeps getting letters pushed into it. the length of this
+	# must be equal to correct_word
+
+# guess_count is a WHILE loop that keeps track of guesses. Once the guesses are up
+# it prints, "You are now out of guesses!" 
+
+# user_feedback prints the progress of guesses in terms of if working word has a 
+# match with correct_word. It will only fill in the blanks if there is a match with
+# correct_word
