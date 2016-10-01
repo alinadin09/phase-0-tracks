8264eddfcef9c5d077fa510ecded3fc0821bc28a
Alina Din <alinadin09@gmail.com>
2016-10-01 14:51:17 -0700

    Need to fix the loops, but user feedback is semi working

--- ruby/game/Game.rb
+++ ruby/game/Game.rb
@@ -10,35 +10,49 @@
 # match with correct_word. It will only fill in the blanks if there is a match with
 # correct_word.
 
-def WordGame
+class Game
+	attr_accessor :correct_word
+	attr_accessor :working_word
+	attr_reader :guess_count
 
 	def initialize
-		@correct_word = ""
-		@working_word = ""
-		@match = false
-		@guess_count_maxed = false
+		@correct_word = correct_word
+		@working_word = working_word
+		@guess_count = 0
 	end
 
 	def add_letter_to_working_word(letter)
-		while @working_word.length != correct_word.length
+			@working_word = []
 			@working_word << letter
-			@guess_count_maxed = false
-
-			if @working_word.length = correct_word.length
-				@guess_count_maxed = true
-			end
-		end
+			p @working_word
 	end
 
 	def user_feedback(letter)
+	counter = 0
 	arr = Array.new(@correct_word.length, "-")
-		if @correct_word.include?(letter)
-		p arr.insert(@correct_word[letter], letter)
-		else
-		p arr
+		while @guess_count <= @correct_word.length
+			puts "Enter another guess"
+			if @correct_word.include?(letter)
+			p new_arr = arr.insert(@correct_word.index(letter), letter)
+			else
+			p arr
+			puts "You have #{guess_count} guesses left"
+			end
 		end
+		counter += 1
 	end
 	
-
-
 end
+
+# driver code / ui
+
+puts "Welcome to the Word Game!"
+game = Game.new
+
+puts "User 1 (aka keeper of secrets) please enter your word:"
+p game.correct_word = gets.chomp.split('')
+
+puts "User 2, please enter your guess:"
+letter = gets.chomp
+p game.add_letter_to_working_word(letter)
+p game.user_feedback(letter)
