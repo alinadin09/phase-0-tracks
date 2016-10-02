4ad6e56f4abde62f9353a5f724fe1132f92da7fd
Alina Din <alinadin09@gmail.com>
2016-10-02 11:01:22 -0700

    Commiting to save previous work that I am about to change

--- ruby/game/Game.rb
+++ ruby/game/Game.rb
@@ -11,48 +11,45 @@
 # correct_word.
 
 class Game
-	attr_accessor :correct_word
-	attr_accessor :working_word
-	attr_reader :guess_count
+	attr_reader :correct_word, :working_word, :guess_count, 
 
-	def initialize
-		@correct_word = correct_word
-		@working_word = working_word
+	def initialize(correct_word)
+		@correct_word = correct_word.split("")
+		@working_word = Array.new(@correct_word.length, "-")
 		@guess_count = 0
+		@all_guesses = []
 	end
 
-	def add_letter_to_working_word(letter)
-			@working_word = []
-			@working_word << letter
-			p @working_word
-	end
+	
 
 	def user_feedback(letter)
-	counter = 0
+	@guess_count = 0
 	arr = Array.new(@correct_word.length, "-")
-		while @guess_count <= @correct_word.length
-			puts "Enter another guess"
-			if @correct_word.include?(letter)
-			p new_arr = arr.insert(@correct_word.index(letter), letter)
-			else
-			p arr
-			puts "You have #{guess_count} guesses left"
+		while @guess_count < @correct_word.length
+		puts "Enter another guess"
+			loop do
+			break if @guess_count = @correct_word.length
+			add_letter_to_working_word(letter)	
+				if @correct_word.include?(letter)
+				p new_arr = arr.insert(@correct_word.index(letter), letter)
+				else
+				p arr
+				puts "You have #{guess_count} guesses left"
+				end
+			@guess_count += 1
 			end
-		end
-		counter += 1
-	end
-	
+		end	
+	end	
 end
 
 # driver code / ui
 
-puts "Welcome to the Word Game!"
-game = Game.new
+
 
 puts "User 1 (aka keeper of secrets) please enter your word:"
-p game.correct_word = gets.chomp.split('')
+correct_word = gets.chomp
+game = Game.new(correct_word)
 
 puts "User 2, please enter your guess:"
 letter = gets.chomp
-p game.add_letter_to_working_word(letter)
 p game.user_feedback(letter)
