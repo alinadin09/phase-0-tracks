3cadf818fb411feaa4624ce60ee89daf0cf85993
Alina Din <alinadin09@gmail.com>
2016-10-17 22:03:13 -0700

    Add fake titles to library

--- databases/public_library/public_library.rb
+++ databases/public_library/public_library.rb
@@ -4,6 +4,7 @@ require 'faker'
 
 # create SQLite3 database
 db = SQLite3::Database.new("library.db")
+db.results_as_hash = true
 
 create_bookshelf = <<-SQL
   CREATE TABLE IF NOT EXISTS library(
@@ -11,3 +12,22 @@ create_bookshelf = <<-SQL
     title VARCHAR(255)
   )
 SQL
+
+# create a bookshelf table
+db.execute(create_bookshelf)
+
+def make_bookshelf(db, title)
+	db.execute("INSERT INTO library (title) VALUES (?)", [title])
+end 
+
+10.times do 
+	make_bookshelf(db, Faker::Book.title)
+	end 
+
+# add a test book
+# db.execute("INSERT INTO library (title) VALUES ('Matilda')")
+
+# books = db.execute("SELECT * FROM library")
+# p books 
+
+
