008ea8655aa7bbacbf3e86f3baaab1e7383decba
Alina Din <alinadin09@gmail.com>
2016-10-17 21:13:15 -0700

    Create bookshelf

--- databases/public_library/public_library.rb
+++ databases/public_library/public_library.rb
@@ -4,3 +4,10 @@ require 'faker'
 
 # create SQLite3 database
 db = SQLite3::Database.new("library.db")
+
+create_bookshelf = <<-SQL
+  CREATE TABLE IF NOT EXISTS library(
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255)
+  )
+SQL
